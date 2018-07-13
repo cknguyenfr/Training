75bbb5b18f80fa61080785c921ddaddb7eef1f89
cknguyenfr <can.khac.nguyen@framgia.com>
2018-07-13 09:08:13 +0700

    Initialize and Image

---
+++ TableViewPj/TableViewPj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewPj
+//
+//  Created by Can Khac Nguyen on 7/13/18.
+//  Copyright © 2018 Can Khac Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
